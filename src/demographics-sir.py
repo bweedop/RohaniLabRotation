41fe4bd6de9448b4824f0e7770c87910865bcd11
bweedop <kbweedop@gmail.com>
2019-09-24 19:29:43 -0400

    Stability notebook added; SIR with demographics added

--- src/demographics-sir.py
+++ src/demographics-sir.py
@@ -3,20 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
 
+# Constant parameters
 beta = 0.75
 gamma = 0.1428
 mu = 0.0003
 
+# Setting up time range
 t0 = 1
 tn = 50*365
 t_step = 1
 t_span = range(t0, tn, t_step)
+plot_time = range(1, 50)
 
+# Initial state parameter values
 s0 = 0.1
 i0 = 0.0001
 r0 = 1 - s0 - i0
 init_params = (s0, i0, r0)
 
+# Differential equations function
 def demographics_sir (params, t):
     res = np.zeros((3))
     res[0] = mu - beta * params[0] * params[1] - mu * params[0]
@@ -24,8 +29,8 @@ def demographics_sir (params, t):
     res[2] = gamma * params[1] - mu * params[2]
     return res
 
+# Solution using the odeint function provided by scipy
 sol = odeint(demographics_sir, init_params, t_span)
-
 
 # Plotting results
 fig, axs = plt.subplots(3)
@@ -33,4 +38,5 @@ fig.suptitle('SIR Model With Demographics')
 axs[0].plot(sol[:,0])
 axs[1].plot(sol[:,1])
 axs[2].plot(sol[:,2])
+axs[2].xlabel('time (days)')
 plt.savefig("demographics-sir.png")
