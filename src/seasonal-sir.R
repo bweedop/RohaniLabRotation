a07d51a37995babdcc87877efaec2fd9ca139468
bweedop <kbweedop@gmail.com>
2019-10-03 08:22:27 -0400

    seasonal plot code added; seasonal-sir.png added

--- src/seasonal-sir.R
+++ src/seasonal-sir.R
@@ -19,23 +19,34 @@ seasonal.sir <- function(t, states, params) {
     i0 <- states[2]
     r0 <- states[3]
 
-    beta <- betafx(beta0 = params[1], beta1 = params[2], t = t)
-    mu <- 0
+    gamma <- params[3]
+    mu <- params[4]
 
-    dS <- mu - beta * s0 * i0 - mu * s0
-    dI <- beta * s0 * i0 - gamma * i0 - mu * i0
+    dS <- mu - betafx(beta0 = params[1], beta1 = params[2], t = t) * s0 * i0 - mu * s0
+    dI <- betafx(beta0 = params[1], beta1 = params[2], t = t) * s0 * i0 - gamma * i0 - mu * i0
     dR <- gamma * i0 - mu * r0
     return(list(c(dS, dI, dR)))
 }
 
-s0 <- 0.9
-i0 <- 0.0001
+s0 <- 0.088
+i0 <- 0.001
 r0 <- 1-s0-i0
 
-t_range <- seq(0, 50*365, 1)
+t_range <- seq(0, 20*365, 1)
 
 initial.states <- c(S = s0, I = i0, R = r0)
 
-params <- c(beta0=1.4, beta1=0.1, gamma = 1/13)
+params <- c(beta0 = 1.4, beta1 = 0.25, gamma = 0.077, mu = 0.0000547945)
 
 out <- ode(y = initial.states, times = t_range, func = seasonal.sir, parms = params)
+
+png("figures/seasonal-sir.png", width = 14, height = 8, units = "in", res=300)
+par(mfrow=c(3,1), pin = c(10, 1.5), mai = c(0, 0.6, 0, 0.1), mar=c(0.5, 4, 2, 0.5), oma = c(0.5, 0, 0, 0.1))
+plot(out[, 2] ~ out[, 1], type="l", col = "blue", xaxt = "n", xlab = "")
+par(mar=c(0.5, 4, 0, 0.5))
+plot(out[, 3] ~ out[, 1], type = "l", col = "red", xaxt = "n", xlab = "")
+par(mar=c(4, 4, 0.5, 0.5))
+plot(out[, 4] ~ out[, 1], type = "l", col = "green", xlab = "time (days)", xaxt = "n")
+xticks <- seq(0, 20*365, 365)
+axis(1, at=xticks)
+dev.off()
